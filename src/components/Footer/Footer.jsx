bbac601763f601fd45d2245e54a544f89c1538ac
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-07 22:22:36 +0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { useLanguage } from '../../LanguageContext';
 import './Footer.css';
 import logoImage from '../../img/logo.png';
 
-const Footer = () => {
-  const { t } = useLanguage();
-
+const Footer = ({ t }) => {
   return (
     <footer className="aline-footer">
       <div className="footer-purple-strip"></div>
@@ -18,13 +15,13 @@ const Footer = () => {
 
           <div className="footer-middle">
             <div className="footer-column">
-              <h3 className="footer-column-title">{t.footerAbout}</h3>
+              <h3 className="footer-column-title">{t.about}</h3>
               <ul className="footer-links">
-                {t.footerLinks.map((link, i) => <li key={i}><a href="#">{link}</a></li>)}
+                {t.links.map((link, i) => <li key={i}><a href="#">{link}</a></li>)}
               </ul>
             </div>
             <div className="footer-column">
-              <h3 className="footer-column-title">{t.footerStore}</h3>
+              <h3 className="footer-column-title">{t.store}</h3>
               <ul className="footer-links">
                 <li><a href="#">Tìm cửa hàng</a></li>
               </ul>
@@ -32,14 +29,14 @@ const Footer = () => {
           </div>
 
           <div className="footer-right">
-            <h3 className="footer-column-title">{t.footerFollow}</h3>
+            <h3 className="footer-column-title">{t.follow}</h3>
             <div className="footer-social-icons">
               <a href="#"><i className='fab fa-facebook-f'></i></a>
               <a href="#"><i className='fab fa-instagram'></i></a>
               <a href="#"><i className='fab fa-tiktok'></i></a>
             </div>
             <div className="footer-map-container">
-              <iframe src="about:blank" width="100%" height="100" style={{border:0}}></iframe>
+              <iframe src="about:blank" width="100%" height="110" style={{border:0}} title="map"></iframe>
             </div>
           </div>
         </div>
