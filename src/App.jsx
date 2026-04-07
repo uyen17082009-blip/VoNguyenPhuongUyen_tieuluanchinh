c3c9145f753ca1f7f8b4aabd3ccb5865c46e1341
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-07 20:44:24 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer";
 
 
