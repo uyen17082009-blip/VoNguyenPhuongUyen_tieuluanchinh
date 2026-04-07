6f04c7a7024cc552f835cc9bf6690f34212c8b66
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-07 21:03:47 +0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-left">
           <div className="footer-logo">
-            {/* SỬA: logoImage thay vì logo */}
+            {/* ĐÃ SỬA: Phải là logoImage giống dòng import ở trên */}
             <img src={logoImage} alt="Aline Beauty" className="footer-logo-img" />
           </div>
           <p className="footer-copyright">
@@ -21,16 +21,8 @@ const Footer = () => {
           <div className="footer-column">
             <h3 className="footer-column-title">Về AlineBeauty</h3>
             <ul className="footer-links">
-              <li><a href="/origin">Nguồn gốc</a></li>
-              <li><a href="/services">Dịch vụ</a></li>
-              <li><a href="/careers">Nghề nghiệp</a></li>
-              <li><a href="/contact">Liên hệ</a></li>
-            </ul>
-          </div>
-          <div className="footer-column">
-            <h3 className="footer-column-title">Hệ thống cửa hàng</h3>
-            <ul className="footer-links">
-              <li><a href="/find-store">Tìm cửa hàng</a></li>
+              <li><a href="/">Trang chủ</a></li>
+              <li><a href="/ve-chung-toi">Về chúng tôi</a></li>
             </ul>
           </div>
         </div>
@@ -38,18 +30,8 @@ const Footer = () => {
         <div className="footer-right">
           <h3 className="footer-column-title">THEO DÕI CHÚNG TÔI</h3>
           <div className="footer-social-icons">
-            <a href="https://facebook.com" className="social-icon"><i className="fab fa-facebook-f"></i></a>
-            <a href="https://instagram.com" className="social-icon"><i className="fab fa-instagram"></i></a>
-          </div>
-          <div className="footer-map" style={{ marginTop: '10px' }}>
-            <a
-              href="https://maps.app.goo.gl/6RuUrqKaYAFapPe57"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-map_link"
-            >
-              Mở trong Google Maps
-            </a>
+            <a href="https://facebook.com" className="social-icon">Facebook</a>
+            <a href="https://instagram.com" className="social-icon">Instagram</a>
           </div>
         </div>
       </div>
