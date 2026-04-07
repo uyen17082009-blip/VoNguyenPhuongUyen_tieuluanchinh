8beb44425c433e42887dcf26de4a4013073067ac
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-07 20:56:40 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
-
-
+import Footer from "./components/Footer/Footer.jsx"; // Thêm dòng này
 
 export default function App() {
   const [page, setPage] = useState("login");
@@ -10,7 +9,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Header />
-
+      {/* Nội dung trang web của bạn sẽ nằm ở đây */}
+      <Footer /> {/* Thêm dòng này */}
     </BrowserRouter>
   );
 }
