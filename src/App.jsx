c1abfd2518e1bbbacbcb58d8ed70d8087aba2a81
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-07 21:04:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 export default function App() {
-  const [page, setPage] = useState("login");
-
   return (
     <BrowserRouter>
       <Header />
-
-      <Footer /> 
+   
+      <Footer />
     </BrowserRouter>
   );
 }
