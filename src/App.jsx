1fb6b3ca6c4aa64ecfad5cb39acdfb6a91ca371d
uyen17082009-blip <uyen17082009@gmail.com>
2026-04-07 21:13:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import "./App.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-   
-      <Footer />
+      <div className="app-wrapper">
+        <Header />
+        {/* main dùng để đẩy footer xuống đáy trang */}
+        <main className="main-content"></main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
